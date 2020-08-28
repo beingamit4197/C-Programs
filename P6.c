e16410f369b438cfafc5517e3c4042dec65e8bb0
Amit Rawat <beingamit4197@gmail.com>
2020-08-28 19:27:15 +0530

    find the size of variables

---
+++ P6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+
+{
+    int a;
+    double b;
+    float c;
+    char d;
+
+    printf("the size of integer %d\n",sizeof(a));
+    printf("the size of double %d\n",sizeof(b));
+    printf("the size of float %f\n",sizeof(c));
+    printf("the size of char %c",sizeof(d));
+
+    getch();
+
+    return 0;
+
+}
