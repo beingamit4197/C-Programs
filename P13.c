97846fc9f091cddaf491469864530775563d4078
Amit Rawat <beingamit4197@gmail.com>
2020-09-01 11:51:42 +0530

    2D Array

---
+++ P13.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+        int sub[2][3],i,j,total=0;
+
+        printf("Enter Six Numbers: ");
+
+        for ( i=0; i<2; i++)
+        {
+            for (j=0; j<3; j++)
+            {
+                scanf("%d",&sub[i][j]);
+            }
+        }
+
+        printf("Values are: ");
+
+        for ( i=0; i<2; i++)
+        {
+            for (j=0; j<3; j++)
+            {
+                printf("\n%d",sub[i][j]);
+                total = total + sub[i][j];
+            }
+                printf("\n Total are: %d",total);
+                total = 0;
+                printf("\n");
+        }
+
+        getch();
+
+}
